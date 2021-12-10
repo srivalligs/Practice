8673a98e283476c6e288beaef7ba400613bffdc3
sriva <sriva@DESKTOP-L20G88V>
2021-12-10 14:26:04 +0530

    edit1

--- src/test/java/Srivalli/SriT1.java
+++ src/test/java/Srivalli/SriT1.java
@@ -7,12 +7,7 @@ public class SriT1 {
 	@Test
 	public void practest() {
 		System.out.println("Hiiiii");
-
-		System.out.println("changes by sowmya");
-
-		System.out.println("Hello");
-		System.out.println("1st time");
-		System.out.println("2nd time");
+		System.out.println("Trial1");
 	}
 
 }
